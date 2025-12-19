27f96a88f4e6c9cab3c4098a869b45c54039b1e6
seiyrikon <rickygalpo7777@gmail.com>
2025-12-19 23:58:10 +0800

    Deployment 2025/19/12

--- src/utils/CommandResult.java
+++ src/utils/CommandResult.java
@@ -2,8 +2,8 @@ package utils;
 
 public class CommandResult {
     public int exitCode;
-    public String output;
-    public String error;
+    public String output = "";
+    public String error= "";
 
     public boolean isSuccess() {
         return exitCode == 0;
